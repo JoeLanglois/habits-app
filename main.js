2b776bc78defe6b6396ee250989c269d0064862a
JoeLanglois <joe.desruisseaux.langlois@gmail.com>
2022-06-18 08:29:38 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,67 @@
+import mitt from 'mitt';
+import { render, html } from 'uhtml';
+
+class AppState {
+  constructor() {
+    this._habits = [];
+    this.e = mitt();
+    this.fetchHabits();
+  }
+
+  on(eventType, handler) {
+    this.e.on(eventType, handler);
+  }
+
+  emit(event, data = {}) {
+    this.e.emit(event, data);
+  }
+
+  off(...args) {
+    this.e.off(...args);
+  }
+
+  get habits() {
+    return this._habits;
+  }
+
+  async fetchHabits() {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        this._habits = [{ name: 'Lire' }, { name: 'Exercise' }];
+        this.e.emit('habits:changed');
+        res();
+      }, 600);
+    });
+  }
+}
+
+const state = new AppState();
+
+class DayView {
+  constructor(state) {
+    this.appState = state;
+    this.el = document.createElement('div');
+
+    this.appState.on('habits:changed', () => this.render());
+
+    this.render();
+  }
+
+  render() {
+    render(this.el, this.view());
+  }
+
+  view() {
+    return html`<p>Number of habits you're tracking: ${
+      this.appState.habits.length
+    }</p>
+    <div>
+      <h2>Habitudes</h2>
+      ${this.appState.habits.map((h) => html`<p>${h.name}</p>`)}
+    </div>
+    `;
+  }
+}
+
+const dayView = new DayView(state);
+document.getElementById('app').append(dayView.el);
